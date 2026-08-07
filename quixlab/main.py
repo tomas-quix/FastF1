54b3e5494d24ce27b91f6d49f2c1b20b777a5585
Tomas Neubauer <tomas@quix.ai>
2026-08-07 12:29:37 +0000

    Update file quixlab/main.py

--- quixlab/main.py
+++ quixlab/main.py
@@ -75,7 +75,7 @@ def ac_telemetry_prod():
     ORDER BY lap, timestamp_ms""")
 
 
-@canvas.ai(position=(1413, -1245), size=(1139, 691), code_height=200, viz={'type': 'line', 'x': 'timestamp', 'y': ['rpms']})
+@canvas.ai(position=(1644, -1382), size=(1139, 691), code_height=200, viz={'type': 'line', 'x': 'timestamp', 'y': ['rpms']})
 def ai_1(ac_telemetry_prod):
     """Plot rpms over timestamp"""
     # ql-ai: generated from prompt 080608f5b1dca70f
