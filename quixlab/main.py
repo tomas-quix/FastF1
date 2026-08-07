7bcb3e9d52d08e180bde05f23530791e1450faa3
Tomas Neubauer <tomas@quix.ai>
2026-08-07 12:48:20 +0000

    Update file quixlab/main.py

--- quixlab/main.py
+++ quixlab/main.py
@@ -73,5 +73,10 @@ def car_telemetry_2():
     ORDER BY ts_ms""")
 
 
+@canvas.cell(position=(1458, -1517), size=(691, 529), code_height=200, viz={'type': 'line', 'x': 'ts_ms', 'y': ['rpm']})
+def cell_2(car_telemetry_2):
+    return car_telemetry_2
+
+
 if __name__ == "__main__":
     canvas.serve()
